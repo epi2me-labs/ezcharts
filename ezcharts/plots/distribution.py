2ceb32382360f9d5dd33e03ba741e02f1efb9856
Rory Munro <rory.munro@nanoporetech.com>
2025-03-26 15:47:54 +0000

    [CW-5885] Implement KDE plot in ezcharts

--- ezcharts/plots/distribution.py
+++ ezcharts/plots/distribution.py
@@ -1,9 +1,10 @@
 """Distributional plots."""
+
 from itertools import cycle
 
-from bokeh.models import HoverTool
+from bokeh.models import ColumnDataSource, HoverTool
 import pandas as pd
-from seaborn._statistics import Histogram
+from seaborn._statistics import Histogram, KDE
 
 from ezcharts.plots import BokehPlot, util
 
@@ -64,8 +65,14 @@ def histplot(
         heights, edges = estimator(variable_data, weights=weights)
 
         plt._fig.quad(
-            top=heights, bottom=0, left=edges[:-1], right=edges[1:],
-            fill_color=color, fill_alpha=opacity, line_color=color, **quad_kwargs
+            top=heights,
+            bottom=0,
+            left=edges[:-1],
+            right=edges[1:],
+            fill_color=color,
+            fill_alpha=opacity,
+            line_color=color,
+            **quad_kwargs,
         )
     plt._fig.y_range.start = 0
     hover = plt._fig.select(dict(type=HoverTool))
@@ -73,9 +80,57 @@ def histplot(
     return plt
 
 
-def kdeplot(*args, **kwargs):
-    """Plot uni/bi-variate distributions using kernel density estimation."""
-    raise NotImplementedError
+def kdeplot(
+    data=None, *, x=None, y=None, hue=None,
+    weights=None, palette=None, hue_order=None,
+    hue_norm=None, color=None, fill=None, multiple=None,
+    common_norm=None, common_grid=None, cumulative=False,
+    bw_method="scott", bw_adjust=1, warn_singular=None,
+    log_scale=None, levels=None, thresh=None,
+    gridsize=200, cut=3, clip=None,
+    legend=None, cbar=None, cbar_ax=None,
+    cbar_kws=None, bokeh_kwargs=None, ax=None,
+    compute=True, **kwargs,
+):
+    """Plot uni-variate distributions using kernel density estimation."""
+    not_implemented = [
+        hue, hue_order, ax, legend, hue_norm, log_scale, levels, warn_singular, thresh,
+        cbar, cbar_ax, cbar_kws, multiple, weights, fill, common_norm, common_grid
+    ]
+    for i in not_implemented:
+        if i is not None:
+            raise NotImplementedError(
+                f"Parameter with the value {i} is not yet implemented."
+            )
+    bokeh_kwargs = bokeh_kwargs if bokeh_kwargs is not None else {}
+
+    plt = BokehPlot(**bokeh_kwargs)
+    if compute:
+        estimate_kws = dict(
+            bw_method=bw_method,
+            bw_adjust=bw_adjust,
+            gridsize=gridsize,
+            cut=cut,
+            clip=clip,
+            cumulative=cumulative,
+        )
+        estimator = KDE(**estimate_kws)
+        density, support = estimator(data, weights=weights)
+    else:
+        density, support = data
+
+    source = ColumnDataSource(
+        data=dict(x=support, y=density)
+    )
+    plt._fig.varea(x='x', y1=0, y2='y', source=source)
+    plt._fig.y_range.start = 0
+
+    hover = plt._fig.select(dict(type=HoverTool))
+    hover.tooltips = [
+            ("Length", "@x{0}"),     # show 3 decimal places
+            ("Density", "@y")  # same for y/KDE value
+        ]
+    return plt
 
 
 def ecdfplot(*args, **kwargs):
