7bd3216599ee7535e04bf81f8c47276c1d5bd0aa
Pablo Gonzalez de la Rosa <pablo.gonzalezdelarosa@nanoporetech.com>
2025-06-16 17:00:01 +0000

    Add sans-serif and arial fonts to ONT report [CW-6270]

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,10 +1,18 @@
 """Test functions in utils."""
 
 from ezcharts import demo
+from ezcharts import ont_demo
 
 
 def test_run_demo():
-    """Run the demo, just as a test that plots all run."""
+    """Run the main demo, just as a test that plots all run."""
     parser = demo.argparser()
     args = parser.parse_args(["--output", "ezcharts_demo_report.html"])
     demo.main(args)
+
+
+def test_run_ont_demo():
+    """Run the ONT specific demo."""
+    parser = ont_demo.argparser()
+    args = parser.parse_args(["--output", "ezcharts_ont_demo_report.html"])
+    ont_demo.main(args)
